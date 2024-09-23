493d6d91edc8653257232c75d96ebddd97c94656
NkwaTambe <nkwajudetambe@gmail.com>
2024-09-23 14:05:05 +0100

    feat: Initialize frontend setup with Vite, React, TypeScript, and PWA for Webank AppManager

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'webank-AppManager',
+      short_name: 'webank-AppManager',
+      description: 'For App Managers!',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
